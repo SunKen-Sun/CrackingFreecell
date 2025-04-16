3f2b7016fb1dc08a732afaa2fac2fd539f0c38b4
DeVante Brooks <dbrooks6@student12.cse.nd.edu>
2025-04-16 16:04:19 -0400

    decode 2.0, can solve with hex but can't retrace or get data from .exe automatically

--- decode.py
+++ decode.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 import re
+import heapq
+import copy
+import time 
+
+suits = {0: "C", "C":0, 1: "D", "D":1, 2: "H", "H":2, 3: "S", "S":3}
+# have both the bit to suit and suit to bit to make access easy and consistent
 
 def decode_card(hex_value):
 
-    suits = {0: "C", 2: "H", 1: "D", 3: "S"}
     ranks = ["A", "2", "3", "4", "5", "6", "7",
              "8", "9", "10", "J", "Q", "K"]
 
@@ -23,14 +28,10 @@ def scan_hex_dump(hex_data):
 
     clean_hex = re.sub(r'[^0-9a-fA-F]', '', hex_data)
 
-    card_values = re.findall(r'[0-9a-fA-F]{2}', clean_hex)
+    card_values = re.findall(r'[0-9a-fA-F]{8}', clean_hex)
 
     results = []
-    skip = 0 # each card is padded by a group of zeros after it is read, this just skips over the zeros so the code doesnt confuse them for ace of clubs
     for value in card_values:
-        if skip>0:
-            skip -=1
-            continue
         num = int(value, 16)
         decoded = decode_card(num)
         if decoded:
@@ -41,13 +42,266 @@ def scan_hex_dump(hex_data):
     return results
 
 def print_cards(cards):
+    for col in cards:
+        for c in col:
+            if not c: continue
+            print(f"{c}", end="\t")
+        print()
+
+def fix_card_structure(cards):
+
+    cascades = [ [] for _ in range(8)]
+    j = -1
     for i, c in enumerate(cards):
-        if i <= 28:
-            if i%7 ==0: print("\n")
-            print(f"{c[1]}", end="\t")
-        else: # new lines for each row/pile of cards that appear
-            if i== 34 or i==40 or  i==46 or i ==53: print("\n")
-            print(f"{c[1]}", end="\t")
+
+        if i < 28:  #first 4 columns
+            col = i // 7
+            pos = i % 7
+        else:  #last 4 columns
+            col = 4 + (i - 28) // 6  # Columns 4-7
+            pos = (i - 28) % 6
+        # cascades[col][pos] = c[1]
+        cascades[col].append(c[1])
+
+    return cascades
+
+def reconstruct_path(final_state, parent_state, final_move, visited):
+    path = []
+    print(len(visited))
+    current_state = final_state
+    current_move = final_move
+    current_parent = parent_state
+    p = 1000
+    return final_state
+
+    # Backtrack until we reach the initial state (parent=None)
+    while current_parent is not None:
+        path.append((current_move, current_state))  # Store (move, resulting_state)
+        # Move up the chain
+        if p <0: break
+        p -=1
+        for prev_state_info in visited:  # Note: Requires storing parents in `visited` (see optimization below)
+            if state_hash(prev_state_info[3]) == state_hash(current_parent):
+                current_move = prev_state_info[5]  # move_used
+                current_parent = prev_state_info[4]  # parent_state
+                break
+        else:
+            break  # Safety check
+
+    path.reverse()  # Order: init -> solution
+    return path
+
+def solve(board):
+    q = []
+    visited = set()
+    
+    init = {"cascade": board["cascade"], "freecell": board["freecell"], "foundation":  board["foundation"]}
+    # (how many cards in foundation, moves took to get here, id of state, then actual state
+    heapq.heappush(q, ( 0, 0, id(init), init, None, None))
+
+    while q:
+        prio, cost, _, state, parent, __ = heapq.heappop(q)
+
+        if solved(state['foundation']): return [state, cost] #reconstruct_path(state, parent, __, visited)
+
+        if state_hash(state) in visited: continue
+        visited.add(state_hash(state))
+
+        for move in valid_moves(state):
+            new_state = make_move(state, move)
+            '''
+            if move['type'] == "free_to_foundation": 
+                #if move['type'] == 'to_foundation' or move['type'] == "free_to_foundation": 
+                #print(f"\n\n{state} -> \n{move} -> \n{new_state}\n\n")
+                #print(f"\n\n{state} -> \n{move} -> \n{new_state}\n\n")
+            '''
+            if state_hash(new_state) not in visited:
+                 
+                heapq.heappush(q, (priority(new_state), cost +1, id(new_state),  new_state, parent, move))
+
+    return None
+
+def priority(state):
+
+    # lower score = better
+    # high priority for having more cards in the foundation and less cards in the free cell
+    s = sum(len(fond) for fond in state['foundation']) 
+    return (-s )
+
+
+def valid_moves(state):
+    moves = []
+    for col, cascade in enumerate(state["cascade"]):
+        if not cascade: continue
+        top_card = cascade[-1]
+        #print(f"top_card: {top_card}")
+        if check_foundation(top_card, state['foundation']):
+            moves.append({  "type": "to_foundation", "from" :  col, "card": top_card})
+
+        if check_empty(state["freecell"]): moves.append({"type": "to_freecell", "from":  col, "card":top_card})
+        # 2 checks of just moving a card to either base foundation or a free cell
+
+        for dst_col, dst_cas in enumerate(state["cascade"]): # trying to move card to each different cascade
+            if col==dst_col: continue
+
+            elif check_stack(top_card, dst_cas): moves.append({"type": "cascade_to_cascade", "from" : col, "to":  dst_col, "card": top_card})
+
+
+    for cell_index, card in enumerate(state["freecell"]): # trying to move freecell cards to foundation or cascade
+        if card is None: continue
+
+        if check_foundation(card, state['foundation']):
+            moves.append({  "type": "free_to_foundation", "from" : cell_index, "card": card})
+
+        for dst_col, dst_cas in enumerate(state["cascade"]):
+            if not dst_cas: continue
+
+            if check_stack(card, dst_cas): moves.append({"type": "free_to_cascade", "from": cell_index, "to": dst_col, "card": card})
+
+    return moves
+
+## NEED TO ACCOUNT FOR: 
+#       automated moves, when you move from cascade to a foundation i think all other potential foundation moves will automatically be made
+
+'''
+    HELPER FUNCTIONS
+    make_move is an orchestrator and calls these helper functions
+    they are what alter a specific part of the state to have the changes that would be made with a given action 
+'''
+def add_to_foundation(card, foundation):
+    foundation[suits[card[-1]]].append(card)
+    
+def remove_from_cascade(origin, cascade):
+    if len(cascade[origin]) ==0:
+        return
+    c = cascade[origin].pop()
+
+def add_to_cascade(card, to, cascade):
+    cascade[to].append(card)
+
+def add_to_freecell(card, freecell):
+    
+    for i,c in enumerate(freecell): 
+        if not c: 
+            freecell[i] = card    
+            break
+
+def remove_from_freecell(card, freecell):
+
+    for i, c in enumerate(freecell): 
+        if c == card: 
+            freecell[i] = None    
+            break
+
+def make_move(state, move):
+
+    '''
+    1) cas to foundation
+    2) cas to freecell
+    3) cas to cas
+    4) freecell to foundation
+    5) freecell to cas
+    '''
+
+    # given the move description, alter the state to reflect what it would be if the state was chosen.
+    move_t = move["type"]
+    card = move["card"]
+    move_f = move["from"]
+    new_state = {"cascade": copy.deepcopy(state["cascade"]), "freecell": state["freecell"].copy(), "foundation": copy.deepcopy(state["foundation"])}
+    
+    if move_t == "to_foundation":
+        add_to_foundation(card, new_state["foundation"])
+        remove_from_cascade(move_f, new_state["cascade"])
+    elif move_t == "to_freecell": 
+        add_to_freecell(card, new_state["freecell"])    
+        remove_from_cascade(move_f, new_state["cascade"])
+    elif move_t == "cascade_to_cascade": 
+        add_to_cascade(card, move["to"], new_state["cascade"])
+        remove_from_cascade(move_f, new_state["cascade"])
+    elif move_t == "free_to_foundation":
+        add_to_foundation(card, new_state["foundation"])
+        remove_from_freecell(card, new_state["freecell"])
+    elif move_t == "free_to_cascade":
+        add_to_cascade(card, move["to"], new_state["cascade"])
+        remove_from_freecell(card, new_state["freecell"])
+    else: print(f"ERROR: {move_t}")
+
+    return new_state
+
+def track(state):
+    # go through the state and backtrack to find the solution
+    pass
+
+def state_hash(state):
+
+    # call a function that will hash the specific state for future use
+    h1 = hash(frozenset(tuple(c) for c in state['cascade']))
+    h2 = hash(frozenset(tuple(c) for c in state['foundation']))
+    h3 = hash(frozenset(tuple(['freecell']))  )
+    return hash(frozenset(tuple([h1, h2, h3])))
+
+
+def solved(foundation):
+
+    if len(foundation[0]) ==13 and len(foundation[1]) ==13 and len(foundation[2]) ==13 and len(foundation[3]) ==13: return True
+
+    return False
+
+def get_card_value(card): # helper function to break card to get its value 
+
+    if card[0] == 'K': return 13
+    if card[0] == 'Q': return 12
+    if card[0] == 'J': return 11
+    if card[0] == 'A': return 1
+    if card[0] == '1': return 10
+    else: return int(card[0])
+
+def descending(card1, card2, check_colors): # card 1 = already placed card, card 2 = card trying to make the move
+    
+    if not card1 or not card2: return False
+
+    val1, suit1 = get_card_value(card1), card1[-1]
+    val2, suit2 = get_card_value(card2), card2[-1]
+
+    if (val1 + 1) != val2: return False 
+    
+    # if we do not need to check for colors, ie for foundation, then just return based on numbers
+    if not check_colors: return True
+
+    # checking for same color 
+    if (suit1 == 'D' or suit1 =='H') and (suit2=='D' or suit2 =='H'): return False 
+    if (suit1 == 'S' or suit1 =='C') and (suit2=='S' or suit2 =='C'): return False 
+    return True
+    
+
+def check_stack(card, dst_cas):
+
+    # descending will automatically check the color and values for us, just need to seperate the dst_cas
+    if len(dst_cas) <1: return True
+    return descending(card, dst_cas[-1], True)
+
+
+def check_foundation(card, foundation):
+    # find the foundation the card should be in, see if the card is +1 to the foundation top
+    # club, diamond, heart, spade: order program will always put stuff in the foundation.
+
+    suit = card[-1]
+    if card[0] =='A': return True 
+    # if we have an ace then automatically know we can place card value
+
+    if ((len(foundation[suits[suit]]) + 1)  == get_card_value(card) ) : 
+        return True
+    #else: print(f"{len(foundation[suits[suit]]) + 1}: {get_card_value(card)}" ) 
+
+    return False
+
+def check_empty(free_cells):
+
+    for cell in free_cells: # just need to check if any free cell is empty
+        if not cell:
+            return True
+
+    return False
 
 
 
@@ -63,14 +317,24 @@ def main():
             break
 
     full_dump = '\n'.join(hex_dump)
-    cards = scan_hex_dump(full_dump)
+    cards = scan_hex_dump(full_dump) # raw string of all the info
 
-    cards=cards[0:52]
+    cards=cards[0:52] # ignore any extra bytes read in from the paste
     # cards printed from back left pile to right pile
     # go from back most card to front most in each pile
-    print_cards(cards)
-    print()
 
+    cards = fix_card_structure(cards)
+    print_cards(cards)
+    board = {"cascade": cards, "freecell": [None for n in range(4)],
+            "foundation": [[] for n in range(4)] }
+    print(board)
+    
+    start = time.time()
+    xop = solve(board)
+    print(len(cards))
+    
+    print(f"got {xop}")
+    print(f"{time.time()-start}")
 
 if __name__ == "__main__":
     main()

b6d6abfab047a2219dad932a99ddc0f1932f8b7d
SunKen-Sun <taetaefett@gmail.com>
2025-04-22 23:54:22 -0400

    super decode + decode combined
    Combines most of the functionality of the 2 decode files so that e2e testing could be done

--- mem_func_attack.py
+++ mem_func_attack.py
@@ -13,40 +13,43 @@ def shell_code_create(hdc, a2, a3, a4, a5):
     in_a3 = a3
     in_a4 = a4
     in_a5 = a5
-    
+   
     shellcode = b''
-    
+   
     shellcode += b'\x68' + struct.pack('<I', in_a5)
-    shellcode += b'\x68' + struct.pack('<I', in_a4)
+    shellcode += b'\x68' + struct.pack('<I', in_a4 & 0xFFFFFFFF)
+    # idk if we should be masking, maybe mask them all? my mask might be wrong
+    #    shellcode += b'\x68' + struct.pack('<I', in_a4)
     shellcode += b'\x68' + struct.pack('<I', in_a3)
     shellcode += b'\x68' + struct.pack('<I', in_a2)
     shellcode += b'\x68' + struct.pack('<I', hdc)
-    
+   
     shellcode += b'\xE8\x00\x00\x00\x00'  # placeholder
-    
+   
     #ret
     shellcode += b'\xC3'
     return shellcode
 
+
 def Inject_shell_code(pm, a2,a3):
-    
+   
   #determining your computer setup you'll need to change the application path
     #pm= pymem.Pymem(r"C:\Users\ludlo\Downloads\Group Project_T2\Group Project_T2\freecell.exe.i64")
    
     #determine a5
-    #accessing active dword_100844C value 
+    #accessing active dword_100844C value
     address = 0x0100844C
     bytes = pm.read_bytes(address, 4)
-    dword_100844C = struct.unpack('i', bytes)[0] 
-    ##accessing active dword_100844C value 
+    dword_100844C = struct.unpack('i', bytes)[0]
+    ##accessing active dword_100844C value
     address = 0x01008DB0
     bytes = pm.read_bytes(address, 4)
-    dword_1008DB0 = struct.unpack('i', bytes)[0] 
+    dword_1008DB0 = struct.unpack('i', bytes)[0]
     #accessing active dword_100814C
     address = 0x0100814C
-    bytes = pm.read_bytes(address, 4); 
+    bytes = pm.read_bytes(address, 4);
     dword_100814C = struct.unpack('i',bytes)[0]
-        
+       
     #Paint.hdc
     address = 0x0019FDF0
     bytes = pm.read_bytes(address,4)
@@ -59,26 +62,34 @@ def Inject_shell_code(pm, a2,a3):
     a3 = input()
     """
     #determine a4
-    address = 0x01008AB0
-    bytes = pm.read_byte(address, ((21*a2)+a3)  * 4)
+    if not a2: a2 = 0
+    if not a3: a3 = 0
+    offset = ((21 * a2) + a3) * 4
+    address = 0x01008AB0 + offset
+    bytes = pm.read_bytes(address, 4)  
     a4 = struct.unpack('i', bytes)[0]
+    # address = 0x01008AB0
+    #bytes = pm.read_bytes(address, ((21*a2)+a3)  * 4)
+    #a4 = struct.unpack('i', bytes)[0]
+
 
     #checks to determine a5
     if(dword_100844C == 1 and a3 == dword_1008DB0 and  a2 == dword_100814C):
         a5 = 2;
     else:
-        a5 = 0; 
+        a5 = 0;
+
 
     inject_shell = shell_code_create(Paint_hdc, a2, a3, a4, a5)
     inject_sz = len(inject_shell)
-
+     
     alloc_address = pymem.memory.allocate_memory(pm.process_handle, inject_sz);
+
 
     if(alloc_address and inject_shell):
        pm.write_bytes(alloc_address, inject_shell, inject_sz)
     else:
        print("Injection failed")
-        
 
 def hex_dump_fo_cards(data, address):
     for i in range(0, len(data), 16):
--- super_decode.py
+++ super_decode.py
@@ -3,15 +3,24 @@
 import re
 import heapq
 import copy
-import time 
-#import pymem
-#import mem_func_attack
+import time
+import pymem
+import mem_func_attack
 
-#pm= pymem.Pymem(r"C:\Users\ludlo\Downloads\Group Project_T2\Group Project_T2\freecell.exe.i64")
+pm= pymem.Pymem(r"freecell.exe")
+
 suits = {0: "C", "C":0, 1: "D", "D":1, 2: "H", "H":2, 3: "S", "S":3}
 # have both the bit to suit and suit to bit to make access easy and consistent
 
 move_id = 0
+# used to keep track of what moves the path actually ended up using
+
+CASCADE_TO_FOUNDATION   = "CAS_TO_FOUND"
+CASCADE_TO_CASCADE      = "CAS_TO_CAS"
+CASCADE_TO_FREE         = "CAS_TO_FREE"
+FREE_TO_CASCADE         = "FREE_TO_CAS"
+FREE_TO_FOUNDATION      = "FREE_TO_FOUND"
+
 
 def decode_card(hex_value):
 
@@ -70,66 +79,41 @@ def fix_card_structure(cards):
 
     return cascades
 
-def reconstruct_path(final_state, parent_state, final_move, visited):
-    path = []
-    print(len(visited))
-    current_state = final_state
-    current_move = final_move
-    current_parent = parent_state
-    p = 1000
-    return final_state
-
-    # Backtrack until we reach the initial state (parent=None)
-    while current_parent is not None:
-        path.append((current_move, current_state))  # Store (move, resulting_state)
-        # Move up the chain
-        if p <0: break
-        p -=1
-        for prev_state_info in visited:  # Note: Requires storing parents in `visited` (see optimization below)
-            if state_hash(prev_state_info[3]) == state_hash(current_parent):
-                current_move = prev_state_info[5]  # move_used
-                current_parent = prev_state_info[4]  # parent_state
-                break
-        else:
-            break  # Safety check
-
-    path.reverse()  # Order: init -> solution
-    return path
-
 def solve(board):
     q = []
     visited = set()
     moves = dict()
-    
+
     init = {"cascade": board["cascade"], "freecell": board["freecell"], "foundation":  board["foundation"], 'moves': dict() }
-    # (how many cards in foundation, moves took to get here, id of state, then actual state
-    heapq.heappush(q, ( 0, 0, id(init), init, None, None))
+    heapq.heappush(q, ( 0, 0, id(init), init, None))
 
+    # heapq has [priority to be sorted by, amount of moves it took to get here, id of state (for uniqueness),  current state, parent state ]
     while q:
-        prio, cost, _, state, parent, __ = heapq.heappop(q)
+        _, cost,___,  state, parent = heapq.heappop(q)
 
-        if solved(state['foundation']): return [state, cost, moves] #reconstruct_path(state, parent, __, visited)
+        if solved(state['foundation']): return [state, cost, moves]
 
         if state_hash(state) in visited: continue
         visited.add(state_hash(state))
-
-        for move in valid_moves(state):
+        vm = valid_moves(state)
+        for move in vm:
             new_state = make_move(state, move)
             if state_hash(new_state) not in visited:
                 global move_id
                 move_id +=1
-                #move['move_id'] = move_id
                 moves[move_id] = move
                 new_state['moves'][move_id] = None
-                heapq.heappush(q, (priority(new_state), cost +1, id(new_state),  new_state, parent, move))
+                if len(vm) ==1: move['len'] = 1
+                # using a dict here because it acts like a set but still keeps the order of stuff to loop over at the end
+                heapq.heappush(q, (priority(new_state), cost +1, id(new_state),  new_state, parent))
 
-    return None
+    return [None, None, None]
 
 def priority(state):
 
     # lower score = better
     # high priority for having more cards in the foundation and less cards in the free cell
-    s = sum(len(fond) for fond in state['foundation']) 
+    s = sum(len(fond) for fond in state['foundation'])
     return (-s )
 
 
@@ -138,43 +122,53 @@ def valid_moves(state):
     for col, cascade in enumerate(state["cascade"]):
         if not cascade: continue
         top_card = cascade[-1]
+        
+        index = [i for i, card in enumerate(state["cascade"]) if card == top_card]
         #print(f"top_card: {top_card}")
         if check_foundation(top_card, state['foundation']):
-            moves.append({  "type": "to_foundation", "from" :  col, "card": top_card})
+            #need to identify the location of suits
+            suit_location = [i for i, card in enumerate(state["foundation"]) if not card or top_card[-1] == card[0][-1]]
+            moves.append({  "type": CASCADE_TO_FOUNDATION, "from" :  col,"c_index": index, "card": top_card, "move_col": 0, "move_ind": suit_location[0]})
 
-        if check_empty(state["freecell"]): moves.append({"type": "to_freecell", "from":  col, "card":top_card})
+        if check_empty(state["freecell"]): 
+            empty_loc = [i for i, card in enumerate(state["freecell"])if card == None]                  
+            moves.append({"type": CASCADE_TO_FREE, "from":  col, "c_index": index, "card":top_card, "move_col":0, "move_ind":empty_loc[0]})
         # 2 checks of just moving a card to either base foundation or a free cell
 
         for dst_col, dst_cas in enumerate(state["cascade"]): # trying to move card to each different cascade
             if col==dst_col: continue
 
-            elif check_stack(top_card, dst_cas): moves.append({"type": "cascade_to_cascade", "from" : col, "to":  dst_col, "card": top_card})
+            elif check_stack(top_card, dst_cas): 
+                index_card = len(state["cascade"][dst_col]) + 1
+                moves.append({"type": CASCADE_TO_CASCADE, "from" : col, "c_index": index, "move_col":  dst_col, "card": top_card, "move_ind": index_card})
 
 
-    for cell_index, card in enumerate(state["freecell"]): # trying to move freecell cards to foundation or cascade
+    for cell_index, card in enumerate(state["freecell"]):# trying to move freecell cards to foundation or cascade
+        index = [i for i, e_card in enumerate(cascade) if e_card == card]
         if card is None: continue
 
         if check_foundation(card, state['foundation']):
-            moves.append({  "type": "free_to_foundation", "from" : cell_index, "card": card})
-
+             suit_location = [i for i, f_card in enumerate(state["foundation"]) if not f_card or  f_card[0][-1] == card[-1]]
+             moves.append({  "type": FREE_TO_FOUNDATION, "from" : cell_index, "c_index": index, "card": card, "move_col":0, "move_ind": suit_location[0]})
         for dst_col, dst_cas in enumerate(state["cascade"]):
+            index_card = len(state["cascade"][dst_col]) + 1
             if not dst_cas: continue
 
-            if check_stack(card, dst_cas): moves.append({"type": "free_to_cascade", "from": cell_index, "to": dst_col, "card": card})
+            if check_stack(card, dst_cas): moves.append({"type": FREE_TO_CASCADE, "from": cell_index, "c_index": index, "move_col": dst_col, "card": card, "move_ind":index_card})
 
     return moves
 
-## NEED TO ACCOUNT FOR: 
+## NEED TO ACCOUNT FOR:
 #       automated moves, when you move from cascade to a foundation i think all other potential foundation moves will automatically be made
 
 '''
     HELPER FUNCTIONS
     make_move is an orchestrator and calls these helper functions
-    they are what alter a specific part of the state to have the changes that would be made with a given action 
+    they are what alter a specific part of the state to have the changes that would be made with a given action
 '''
 def add_to_foundation(card, foundation):
     foundation[suits[card[-1]]].append(card)
-    
+
 def remove_from_cascade(origin, cascade):
     if len(cascade[origin]) ==0:
         return
@@ -184,17 +178,17 @@ def add_to_cascade(card, to, cascade):
     cascade[to].append(card)
 
 def add_to_freecell(card, freecell):
-    
-    for i,c in enumerate(freecell): 
-        if not c: 
-            freecell[i] = card    
+
+    for i,c in enumerate(freecell):
+        if not c:
+            freecell[i] = card
             break
 
 def remove_from_freecell(card, freecell):
 
-    for i, c in enumerate(freecell): 
-        if c == card: 
-            freecell[i] = None    
+    for i, c in enumerate(freecell):
+        if c == card:
+            freecell[i] = None
             break
 
 def make_move(state, move): # simulates being at state1, taking move, then ending at state2. there is no gurantee that state2 is actually needed for the final solution though
@@ -212,21 +206,21 @@ def make_move(state, move): # simulates being at state1, taking move, then endin
     card = move["card"]
     move_f = move["from"]
     new_state = {"cascade": copy.deepcopy(state["cascade"]), "freecell": state["freecell"].copy(), "foundation": copy.deepcopy(state["foundation"]), 'moves': state['moves'].copy()}
-    
-    if move_t == "to_foundation":
+
+    if move_t == CASCADE_TO_FOUNDATION:
         add_to_foundation(card, new_state["foundation"])
         remove_from_cascade(move_f, new_state["cascade"])
-    elif move_t == "to_freecell": 
-        add_to_freecell(card, new_state["freecell"])    
+    elif move_t == CASCADE_TO_FREE:
+        add_to_freecell(card, new_state["freecell"])
         remove_from_cascade(move_f, new_state["cascade"])
-    elif move_t == "cascade_to_cascade": 
-        add_to_cascade(card, move["to"], new_state["cascade"])
+    elif move_t == CASCADE_TO_CASCADE:
+        add_to_cascade(card, move["move_col"], new_state["cascade"])
         remove_from_cascade(move_f, new_state["cascade"])
-    elif move_t == "free_to_foundation":
+    elif move_t == FREE_TO_FOUNDATION:
         add_to_foundation(card, new_state["foundation"])
         remove_from_freecell(card, new_state["freecell"])
-    elif move_t == "free_to_cascade":
-        add_to_cascade(card, move["to"], new_state["cascade"])
+    elif move_t == FREE_TO_CASCADE:
+        add_to_cascade(card, move["move_col"], new_state["cascade"])
         remove_from_freecell(card, new_state["freecell"])
     else: print(f"ERROR: {move_t}")
 
@@ -251,32 +245,32 @@ def solved(foundation):
 
     return False
 
-def get_card_value(card): # helper function to break card to get its value 
+def get_card_value(card): # helper function to break card to get its value
 
     if card[0] == 'K': return 13
     if card[0] == 'Q': return 12
     if card[0] == 'J': return 11
     if card[0] == 'A': return 1
-    if card[0] == '1': return 10
+    if card[0] == '1': return 10  # if card is 10, ie 10D then will start with 1 -> return 10. if card is an ace it will be AD so no need to worry about other first chars being a 1
     else: return int(card[0])
 
 def descending(card1, card2, check_colors): # card 1 = already placed card, card 2 = card trying to make the move
-    
+
     if not card1 or not card2: return False
 
     val1, suit1 = get_card_value(card1), card1[-1]
     val2, suit2 = get_card_value(card2), card2[-1]
 
-    if (val1 + 1) != val2: return False 
-    
+    if (val1 + 1) != val2: return False
+
     # if we do not need to check for colors, ie for foundation, then just return based on numbers
     if not check_colors: return True
 
-    # checking for same color 
-    if (suit1 == 'D' or suit1 =='H') and (suit2=='D' or suit2 =='H'): return False 
-    if (suit1 == 'S' or suit1 =='C') and (suit2=='S' or suit2 =='C'): return False 
+    # checking for same color
+    if (suit1 == 'D' or suit1 =='H') and (suit2=='D' or suit2 =='H'): return False
+    if (suit1 == 'S' or suit1 =='C') and (suit2=='S' or suit2 =='C'): return False
     return True
-    
+
 
 def check_stack(card, dst_cas):
 
@@ -290,12 +284,11 @@ def check_foundation(card, foundation):
     # club, diamond, heart, spade: order program will always put stuff in the foundation.
 
     suit = card[-1]
-    if card[0] =='A': return True 
+    if card[0] =='A': return True
     # if we have an ace then automatically know we can place card value
 
-    if ((len(foundation[suits[suit]]) + 1)  == get_card_value(card) ) : 
+    if ((len(foundation[suits[suit]]) + 1)  == get_card_value(card) ) :
         return True
-    #else: print(f"{len(foundation[suits[suit]]) + 1}: {get_card_value(card)}" ) 
 
     return False
 
@@ -304,14 +297,13 @@ def check_empty(free_cells):
     for cell in free_cells: # just need to check if any free cell is empty
         if not cell:
             return True
-
     return False
 
 
 
 def main():
     print("paste hex dump (ctrl+D to finish):")
-    
+
     hex_dump = []
     while True:
         try:
@@ -332,16 +324,26 @@ def main():
     board = {"cascade": cards, "freecell": [None for n in range(4)],
             "foundation": [[] for n in range(4)] }
     print(board)
-    
+
     start = time.time()
     xop, cost, moves = solve(board)
     print(len(cards))
-    
+
     print(f"got {xop}")
     print(f"{cost} vs {len(xop['moves'])} vs {len(moves)}")
     print(f"{time.time()-start}")
-    for i,id in enumerate(xop['moves']): ## actually finding 
+    for i,id in enumerate(xop['moves']): ## actually finding
+        move  = moves[id]
+        mem_func_attack.Inject_shell_code(pm,move['from'], move['c_index'])
         print(f"{i}, {id} - {moves[id]}")
+        time.sleep(0.1) # wait a tiny bit between injections just in case there is some clock time to consider
+
+        mem_func_attack.Inject_shell_code(pm,move['move_col'], move['move_ind'])
+        time.sleep(0.1)
+        mem_func_attack.identify_message_box(pm);
+        time.sleep(0.1)
+
+
 
 if __name__ == "__main__":
     main()
